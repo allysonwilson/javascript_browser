728496f26ad73829a408310bc8fdedf36917f551
Allyson Gayle Wilson <allysonmacwilson@gmail.com>
2017-10-16 21:35:50 +0100

    addCat function

--- javascript_browser/cats_homework_start_point/public/app.js
+++ javascript_browser/cats_homework_start_point/public/app.js
@@ -1,13 +1,42 @@
-var catList = document.createElement('cats');
+// var cats_array = [
+//   { cat: },
+//   { cat: },
+//   { cat: },
+//   { cat: }
+// ]
+
+
+var createCatList = function () {
+ var catList = document.createElement('cats');
  catList.classList.add('cat');
 
-var addCat = function (name, favouriteFood, image){
-  var
+ var catName = document.createElement ('cat name');
+ catName.innerText = "A particular Kitty";
+
+ var catFood = document.createElement ('favourite food');
+ catFood.innerText = "yum yums";
+
+ var catImage = document.createElement ('cat pic');
+ catImage.innerText = "pretty kitty";
+
+  catFood.appendChild(catName);
+  catName.appendChild(catFood);
+  catList.appendChild(catName);
+
+  var catListing = document.querySelector('#catListing');
+  catListing.appendChild(catList);
+
 }
-// unordered list <ul> id="cats">  -- class="cat" each section name-- favouriteFood-- image
+
+window.onload = app;
+
+// Add JS code to allow a new cat to be added programmatically. Do it rough and ready at first!
+// Refactor the code to make it easy to add a new cat, just like we did with the quotes.
+// EXTENSION: Remove the cat data in the HTML and add them via your javascript. If you want to be fancy, put the cat data into an array.
+// EXTENSION: Style the site!
 
 
-//  +1. Add JS code to allow a new cat to be added programmatically. Do it rough and ready at first!
-//  +2. Refactor the code to make it easy to add a new cat, just like we did with the quotes.
+
+
 //  +3. EXTENSION: Remove the cat data in the HTML and add them via your javascript. If you want to be fancy, put the cat data into an array.
 //  +4. EXTENSION: Style the site!
