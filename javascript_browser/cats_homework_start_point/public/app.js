48ff0c45f1ec60241a67a82c46abe7572b086e43
Allyson Gayle Wilson <allysonmacwilson@gmail.com>
2017-10-16 20:57:20 +0100

    creating parent container

---
+++ javascript_browser/cats_homework_start_point/public/app.js
@@ -0,0 +1,13 @@
+var catList = document.createElement('cats');
+ catList.classList.add('cat');
+
+var addCat = function (name, favouriteFood, image){
+  var
+}
+// unordered list <ul> id="cats">  -- class="cat" each section name-- favouriteFood-- image
+
+
+//  +1. Add JS code to allow a new cat to be added programmatically. Do it rough and ready at first!
+//  +2. Refactor the code to make it easy to add a new cat, just like we did with the quotes.
+//  +3. EXTENSION: Remove the cat data in the HTML and add them via your javascript. If you want to be fancy, put the cat data into an array.
+//  +4. EXTENSION: Style the site!
